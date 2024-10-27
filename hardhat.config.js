d0a37f120dee3f7a7ce83df768fd82f2e3a034bc
arslan-codes <arslanmazharnfts@gmail.com>
2024-10-27 22:21:51 +0500

    contract deployed to sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,30 +1,25 @@
-require("@nomiclabs/hardhat-ethers");
+// module.exports = {
+//   solidity: "0.8.27",
+//   networks: {
+//     localhost: {
+//       url: "http://127.0.0.1:8545",
+//     },
+//   },
+// };
 
-module.exports = {
-  solidity: "0.8.27",
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
-  },
-};
-
-/*
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.19",
   networks: {
     hardhat: {},
     sepolia: {
-      url: process.env.SEPOLIA_URL,
-      accounts: [process.env.PRIVATE_KEY]
-    }
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.SEPOLIA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  }
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY
+  // }
 };
-
-*/
