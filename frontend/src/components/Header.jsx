2f043c42c636c1204c2d4636e3d1a8353b076099
arslan-codes <arslanmazharnfts@gmail.com>
2024-11-01 23:51:01 +0500

    updated ui of dbank

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,40 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => setIsOpen(!isOpen);
-
   return (
     <div className="flex justify-center">
       <div
-        className="fixed top-2 z-50 bg-gradient-to-r m-2 p-1 from-pink-900 bg-opacity-30
-       backdrop-blur-lg flex justify-between w-3/4 rounded-full"
+        className="md: w-4/5   lg:  fixed top-2 z-50  m-2 p-1 bg-gradient-to-r from-pink-900 bg-opacity-30
+        backdrop-blur-lg flex justify-center  rounded-full"
       >
-        {/* Toggle Button for Mobile */}
-        <button className="md:hidden text-white" onClick={toggleMenu}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
-        </button>
-
-        {/* Desktop Links */}
-        <div className="hidden md:flex gap-32">
+        <div className="flex flex-row  gap-0    lg:flex-row md:gap-32">
           <RouterLink
             to="/"
-            className="btn btn-ghost text-l text-white cursor-pointer md:text-xs"
+            smooth={true}
+            duration={500}
+            className="btn btn-ghost text-sm text-white cursor-pointer md:text-lg"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -42,7 +21,7 @@ const Header = () => {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="size-6"
+              className="size-6 "
             >
               <path
                 strokeLinecap="round"
@@ -50,7 +29,7 @@ const Header = () => {
                 d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
               />
             </svg>
-            Home
+            <p className="hidden sm:block">Home</p>
           </RouterLink>
           <RouterLink
             to="/getStarted"
@@ -62,7 +41,7 @@ const Header = () => {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="size-6"
+              className="size-6 "
             >
               <path
                 strokeLinecap="round"
@@ -70,7 +49,7 @@ const Header = () => {
                 d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
               />
             </svg>
-            About
+            <p className="hidden sm:block"> About</p>
           </RouterLink>
           <RouterLink to="/blog" className="btn btn-ghost text-l text-white">
             <svg
@@ -79,7 +58,7 @@ const Header = () => {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="size-6"
+              className="size-6 "
             >
               <path
                 strokeLinecap="round"
@@ -87,16 +66,19 @@ const Header = () => {
                 d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z"
               />
             </svg>
-            Articles
+            <p className="hidden sm:block"> Articles</p>
           </RouterLink>
-          <RouterLink to="/contact" className="btn btn-ghost text-l text-white">
+          <RouterLink
+            to="/contact"
+            className="btn btn-ghost text-sm text-white md:text-lg"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="size-6"
+              className="size-6 "
             >
               <path
                 strokeLinecap="round"
@@ -104,43 +86,9 @@ const Header = () => {
                 d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
               />
             </svg>
-            Contact
+            <p className="hidden sm:block"> Contact</p>
           </RouterLink>
         </div>
-
-        {/* Mobile Menu */}
-        {isOpen && (
-          <div className="absolute top-10 rounded-full left-0 right-0 z-50 flex flex-row items-center bg-gradient-to-r from-pink-900 bg-opacity-80 md:hidden">
-            <RouterLink
-              to="/"
-              className="btn btn-ghost text-l text-white py-2"
-              onClick={toggleMenu}
-            >
-              Home
-            </RouterLink>
-            <RouterLink
-              to="/getStarted"
-              className="btn btn-ghost text-l text-white py-2"
-              onClick={toggleMenu}
-            >
-              About
-            </RouterLink>
-            <RouterLink
-              to="/blog"
-              className="btn btn-ghost text-l text-white py-2"
-              onClick={toggleMenu}
-            >
-              Articles
-            </RouterLink>
-            <RouterLink
-              to="/contact"
-              className="btn btn-ghost text-l text-white py-2"
-              onClick={toggleMenu}
-            >
-              Contact
-            </RouterLink>
-          </div>
-        )}
       </div>
     </div>
   );
