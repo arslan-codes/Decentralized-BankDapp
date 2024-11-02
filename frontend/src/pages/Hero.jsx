fb737daf52eb6fb1e85690ab0bd7d89b6bf14080
arslan-codes <arslanmazharnfts@gmail.com>
2024-11-03 00:05:14 +0500

    possible ui updated

--- frontend/src/pages/Hero.jsx
+++ frontend/src/pages/Hero.jsx
@@ -26,11 +26,9 @@ const Hero = () => {
             </a> */}
             <button
               type="button"
-              class="text-gray-900 bg-white hover:bg-pink-700 border 
-               focus:ring-4 focus:outline-none focus:ring-gray-100 
+              class="text-gray-900  hover:bg-pink-900 bg-pink-100  
                font-semibold hover:text-white rounded-lg text-sm px-5 
-               py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600
-                dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2"
+               py-2.5 text-center inline-flex items-center me-2 mb-2"
             >
               <svg
                 aria-hidden="true"
