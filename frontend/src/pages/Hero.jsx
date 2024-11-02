ceaf0b56cbb1e48d24d29f454fc5045317cf8863
arslan-codes <arslanmazharnfts@gmail.com>
2024-11-02 22:36:21 +0500

    withdraw page

--- frontend/src/pages/Hero.jsx
+++ frontend/src/pages/Hero.jsx
@@ -7,24 +7,9 @@ const Hero = () => {
 
   return (
     <Layout>
-      {" "}
-      <div className="bg-black text-white min-h-screen ">
-        <div className="relative isolate px-6 ">
-          <div
-            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-            ></div>
-          </div>
-          <div className="mx-auto max-w-2xl py-28 sm:py-48 lg:py-30">
-            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-              {/* <div className="relative rounded-full px-3 py-1 text-sm leading-6  text-white ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+      <div className="mx-auto max-w-2xl py-28 sm:py-48 lg:py-30">
+        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+          {/* <div className="relative rounded-full px-3 py-1 text-sm leading-6  text-white ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 Announcing our next round of funding.{" "}
                 <a
                   href="#blog"
@@ -35,30 +20,28 @@ const Hero = () => {
                   Read more <span aria-hidden="true">&rarr;</span>
                 </a>
               </div> */}
-            </div>
-            <div className="text-center">
-              <h1 className="text-2xl sm: text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                Decentralized Bank
-              </h1>
-              <p className="text-sm md:text-lg mt-2  md:mt-6 leading-8 text-gray-100 whitespace-pre-wrap sm:whitespace-normal">
-                The future of finance is here. Manage your cryptocurrencies and
-                traditional assets in one secure platform.
-              </p>
-              <div className="mt-6 md:mt-10 flex flex-col sm:flex-row items-center justify-center gap-x-6">
-                <a
-                  href="#"
-                  className="rounded-md bg-pink-600 px-2 sm:px-3.5  py-1.5 sm:py-2.5 text-xs sm:text-sm font-semibold text-white shadow-sm hover:bg-pink-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 focus-cursor-mouse"
-                >
-                  Connect Wallet
-                </a>
-                {/* <a
+        </div>
+        <div className="text-center">
+          <h1 className="text-2xl sm: text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            Decentralized Bank
+          </h1>
+          <p className="text-sm md:text-lg mt-2  md:mt-6 leading-8 text-gray-100 whitespace-pre-wrap sm:whitespace-normal">
+            The future of finance is here. Manage your cryptocurrencies and
+            traditional assets in one secure platform.
+          </p>
+          <div className="mt-6 md:mt-10 flex flex-col sm:flex-row items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-pink-600 px-2 sm:px-3.5  py-1.5 sm:py-2.5 text-xs sm:text-sm font-semibold text-white shadow-sm hover:bg-pink-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 focus-cursor-mouse"
+            >
+              Connect Wallet
+            </a>
+            {/* <a
                   href="#"
                   className="text-xs md:text-xs font-semibold leading-6 text-white"
                 >
                   Learn more <span aria-hidden="true">→</span>
                 </a> */}
-              </div>
-            </div>
           </div>
         </div>
       </div>
--- frontend/src/pages/Withdraw.jsx
+++ frontend/src/pages/Withdraw.jsx
@@ -1,7 +1,76 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+import SwapForm from "./SwapFrom";
 const Withdraw = () => {
-  return <div>Withdraw</div>;
+  const [Balance, setBalance] = useState(0);
+  return (
+    <Layout>
+      <div className="mx-auto max-w-2xl py-14 sm:py-28 lg:py-30">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-4 text-center text-2xl/9 font-bold tracking-tight text-white">
+            Withdraw Your Funds
+            <h1 className="text-base md:text-lg text-center font-semibold text-gray-100 mb-6">
+              anytime, anywhere.
+            </h1>
+          </h2>
+        </div>
+        {/* <SwapForm></SwapForm> */}
+        <div className="flex flex-col md:flex-row  md:space-x-6  m-6 justify-center ">
+          <SwapForm>
+            {/* Sell Section */}
+
+            {/* Buy Section */}
+            <div className="flex justify-start items-center mb-4">
+              <div className="w-full">
+                <p className="text-gray-900 text-base sm:text-2xl font-semibold my-1 sm:my-2">
+                  Amount
+                </p>
+                <input
+                  type="text"
+                  placeholder="0 Eth"
+                  className="text-base  border-2 border-gray-200    rounded-2xl 
+                  sm:text-2xl w-full form-control font-semibold text-gray-800  
+                  py-3 sm:py-6   ring-black  focus:rounded-2xl px-2 sm:px-4
+                   focus:ring-blue-500 focus:border-blue-500 block "
+                />
+              </div>
+            </div>
+            <div className="flex justify-center">
+              <button className="w-3/4  sm:w-full bg-pink-200 text-pink-600 font-semibold py-3 rounded-lg mt-4">
+                Withdraw
+              </button>
+            </div>
+            {/* Get Started Button */}
+          </SwapForm>
+          <SwapForm>
+            {/* Sell Section */}
+            <div className="flex justify-start items-center mb-4">
+              <div className="w-full">
+                <p className="text-gray-600 text-sm sm:text-2xl font-semibold my-1 sm:my-2">
+                  Current Balance <p className="text-gray-900">{Balance}-Eth</p>
+                </p>
+              </div>
+            </div>
+
+            {/* Buy Section */}
+            <div className="flex justify-start items-center mb-4">
+              <div className="w-full">
+                <p className="text-gray-900 text-base sm:text-2xl font-semibold my-1 sm:my-2">
+                  Previous Withdraws
+                </p>{" "}
+                <div>
+                  {" "}
+                  <p className="text-black">All Withdraws</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Get Started Button */}
+          </SwapForm>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Withdraw;
