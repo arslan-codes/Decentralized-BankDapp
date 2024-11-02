9be2baf9be76ab0234a0d9948743bd3b4ba312e2
arslan-codes <arslanmazharnfts@gmail.com>
2024-11-02 22:57:21 +0500

    button reset

--- frontend/src/pages/SwapFrom.jsx
+++ frontend/src/pages/SwapFrom.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const SwapForm = ({ children }) => {
   return (
     <div className="flex flex-col items-center mt-3">
-      <div className="bg-white  flex flex-col rounded-xl shadow-lg p-6 w-3/4 h-70 sm:h-96 sm:w-96 max-w-md">
+      <div
+        className="bg-white  flex flex-col rounded-xl  p-6 w-3/4 h-70 sm:h-full sm:w-96 
+      max-w-md focus:ring-4 focus:outline-none focus:ring-purple-300 shadow-lg shadow-pink-500/50"
+      >
         {children}
       </div>
     </div>
